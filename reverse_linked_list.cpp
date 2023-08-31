f91b1f222ad232b09c953ddbdcf676ceb4d2690d
SAI KRISHNA KUMAR REDDY YADAMAKANTI <46914419+saikrishnakumarreddy@users.noreply.github.com>
2023-08-31 23:49:30 +0530

    reverse linked list
    implemented in 2 ways
    1. simple approach
    2. recursive approach

---
+++ reverse_linked_list.cpp
@@ -0,0 +1,90 @@
+/* reverse linked List 
+ * 1. simple reverse
+ * 2. recursive approach
+ * 3. stack based approach (Not implemented here) 
+ */
+
+
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+    
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class LinkedList {
+    public:
+    Node* head;
+    LinkedList() {
+        head = NULL;
+    }
+    
+    void print() {
+        Node* temp = head;
+        while(temp != NULL) {
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+        cout<<"\n";
+    }
+    
+    void push(int val) {
+        Node* temp = new Node(val);
+        temp->next = head;
+        head = temp;
+    }
+    
+    void simpleReverse() {
+        Node* prev = NULL;
+        Node* next = NULL;
+        Node* curr = head;
+        
+        while(curr != NULL) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+    
+    Node* recursiveReverse(Node *node) {
+        if (node == NULL) {
+            return NULL;
+        }
+        if (node->next == NULL) {
+            head = node;
+            return node;
+        }
+        Node* node1  = recursiveReverse(node->next);
+        node1->next = node;
+        node->next = NULL;
+        return node;
+        
+    }
+};
+
+int main() {
+    LinkedList ll;
+    ll.push(1);
+    ll.push(7);
+    ll.push(4);
+    ll.push(9);
+    ll.push(3);
+    ll.push(6);
+    
+    ll.print();
+    ll.recursiveReverse(ll.head);
+    ll.print();
+    
+    ll.simpleReverse();
+    ll.print();
+    
+}
