60a7c44245357e69a4d53fd5744068c49f4c3880
SAI KRISHNA KUMAR REDDY YADAMAKANTI <46914419+saikrishnakumarreddy@users.noreply.github.com>
2023-09-23 18:56:42 +0530

    merge sorting

--- Searching and Sorting/merge_sort.cpp
+++ Searching and Sorting/merge_sort.cpp
@@ -1,1 +1,68 @@
+#include<iostream>
+using namespace std;
 
+void printArray(int arr[], int size) {
+    for (int i=0; i<size; i++) {
+        cout<< arr[i] <<" ";
+    }
+    cout<<endl;
+}
+
+void merge(int arr[], int begin, int mid, int end) {
+    int i = begin;
+    int j = mid+1;
+    int k = begin;
+
+    int size = end - begin + 1;
+    int b[size];
+    while( i<=mid && j<= end) {
+        if (arr[i] < arr[j]) {
+            b[k] = arr[i];
+            i++;
+        } else {
+            b[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+    if (i>mid) {
+        while(j<=end) {
+            b[k] = arr[j];
+            k++;
+            j++;
+        }
+    }
+    if (j>end) {
+        while(i<=mid) {
+            b[k] = arr[i];
+            k++;
+            i++;
+        }
+    }
+    for(int k=begin; k<=end; k++) {
+        arr[k] = b[k];
+    }
+}
+
+void mergeSort(int arr[], int begin, int end) {
+    if (begin >= end) {
+        return;
+    }
+    int mid = (begin + end) / 2;
+    mergeSort(arr, begin, mid);
+    mergeSort(arr, mid+1, end);
+
+    merge(arr, begin, mid, end);
+}
+
+int main() {
+    int arr[] = {9,4,7,6,3,1,5};
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+
+    printArray(arr,arr_size);
+
+    mergeSort(arr, 0, arr_size-1);
+
+    printArray(arr,arr_size);
+
+}
