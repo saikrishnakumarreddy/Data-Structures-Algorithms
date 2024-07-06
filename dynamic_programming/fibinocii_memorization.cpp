4fd67b9c508d9a3bc9f072f05c9a5c2659078c1d
SAI KRISHNA KUMAR REDDY YADAMAKANTI <46914419+saikrishnakumarreddy@users.noreply.github.com>
2024-07-06 11:21:29 +0530

    Create fibinocii_memorization.cpp

---
+++ dynamic_programming/fibinocii_memorization.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> mem(100,-1);
+ 
+int fib(int n) {
+    if(mem[n] != -1){
+        return mem[n];
+    }
+    if (n==0 || n==1) {
+        mem[n]=n;
+        return mem[n];
+    }
+    mem[n] = fib(n-1) + fib(n-2);
+    return mem[n];
+}
+
+int main() {
+    cout<<fib(4);
+    return 0;
+}
